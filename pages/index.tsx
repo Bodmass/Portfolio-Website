e5409e715a80a18aaf0494bc1540d33ee8b08719
Aziz <ararwow@gmail.com>
2020-10-16 13:21:33 +0100

    feat: update card -removed button from card components -added page parameter on card -changed card to be the project link instead of button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import Button from '../components/Button'
 
-function Card({ children, image }) {
+function Card({ children, image, page }) {
   return (
-    <div className="card">
+    <a href={page} className="card">
       <img className="cardimg" draggable={false} src={image} alt="" />
       <div className="cardcontent">{children}</div>
-    </div>
+    </a>
   )
 }
 
@@ -21,38 +20,42 @@ export default function Home() {
       </Head>
       <Header />
       <div className="content">
+        <div className="aboutme">
+          <p>
+            Hey, I&apos;m Aziz!
+            <br /> I enjoy making games, particularly RPGs, taking part in game jams,
+            <br />
+            and creating useful functional web apps.
+          </p>
+        </div>
+        <hr />
         <div className="cards">
-          <Card image="../images/cards/leap.jpg">
+          <Card image="../images/cards/leap.jpg" page="#">
             <p>Game Project: C#, Unity</p>
             <ul>
               <li>2D Action RPG</li>
               <li>Imbue the power of various elements with the help of Fairies you meet</li>
               <li>Wield a time-altering polearm artifact to view the events of the Great Levian War</li>
             </ul>
-
-            <Button href="#">View Project</Button>
           </Card>
-          <Card image="../images/cards/ld45.jpg">
+          <Card image="../images/cards/ld45.jpg" page="https://bodmassad.itch.io/ld45">
             <p>Game Project: C#, Unity</p>
             <ul>
               <li>Game Jam: Ludum Dare 45</li>
               <li>Theme: Start with nothing</li>
               <li>You have no resources and need to gather materials to craft more drones to protect you!</li>
             </ul>
-
-            <Button href="https://bodmassad.itch.io/ld45">Play on Itch.io</Button>
           </Card>
 
-          <Card image="../images/cards/snakegame.jpg">
+          <Card image="../images/cards/snakegame.jpg" page="">
             <p>Game Project: C++, SFML</p>
             <ul>
               <li>2 Player Snake Game</li>
               <li>Try gain the highest score</li>
               <li>Gather glue to freeze yourself tactically to kill the enemy snake!</li>
             </ul>
-            <Button href="#">Read More</Button>
           </Card>
-          <Card image="../images/cards/hapivania.jpg">
+          <Card image="../images/cards/hapivania.jpg" page="">
             <p>Game Engine: C++, HAPI</p>
             <p>Level Editor: C#, WPF</p>
             <ul>
@@ -60,10 +63,9 @@ export default function Home() {
               <li>Create Levels in the Level Editor (AzEditor)</li>
               <li>Place down and collect upgrades!</li>
             </ul>
-            <Button href="#">Read More</Button>
           </Card>
 
-          <Card image="../images/cards/tilemapgenerator.jpg">
+          <Card image="../images/cards/tilemapgenerator.jpg" page="">
             <p>Unity Plugin: C#, Unity</p>
             <ul>
               <li>2D Level Generator</li>
@@ -74,15 +76,13 @@ export default function Home() {
               <li>Generate foliage and foliage density.</li>
               <li>Import your own tiles to be used in the generated level</li>
             </ul>
-            <Button href="#">Read More</Button>
           </Card>
-          <Card image="../images/cards/ffxivsssdps.jpg">
-            <p>FFXIV App: Javascript</p>
+          <Card image="../images/cards/ffxivsssdps.jpg" page="">
+            <p>FFXIV App: React, Javascript</p>
             <ul>
               <li>Calculates DPS based on Stone Sky and Sea attempts</li>
               <li>Select Boss Fights from Shadowbringers and Legacy</li>
             </ul>
-            <Button href="#">View Project</Button>
           </Card>
         </div>
       </div>
